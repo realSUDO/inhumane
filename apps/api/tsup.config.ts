e39b81d27ad20d4bf06653426f2a38331aebb050
Sumit Vishwkarma <justmultiplythinks@gmail.com>
2026-06-15 23:54:43 +0530

    [FIX] : Mark pg as external in tsup , add to api deps

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["./src/index.ts"],
   noExternal: [/@repo\/.*/],
+  external: ["pg"],
   splitting: false,
   bundle: true,
   outDir: "./dist",
